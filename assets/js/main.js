7b2ac4cd13f5dc5892b2860af0279d9009689dc1
Gautam Sachdeva <gautamsachdeva20@gmail.com>
2024-07-11 15:13:13 +0530

    Parallax effect added to about img

--- assets/js/main.js
+++ assets/js/main.js
@@ -231,3 +231,88 @@ sr.reveal(`.footer, footer__container`, {
   origin: "bottom",
   distance: "30px",
 });
+
+// Parallax tilt effect to cards
+
+class parallaxTiltEffect {
+
+  constructor({element, tiltEffect}) {
+
+    this.element = element;
+    this.container = this.element.querySelector(".container--4");
+    this.size = [300, 360];
+    [this.w, this.h] = this.size;
+
+    this.tiltEffect = tiltEffect;
+
+    this.mouseOnComponent = false;
+
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+    this.defaultStates = this.defaultStates.bind(this);
+    this.setProperty = this.setProperty.bind(this);
+    this.init = this.init.bind(this);
+
+    this.init();
+  }
+
+  handleMouseMove(event) {
+    const {offsetX, offsetY} = event;
+
+    let X;
+    let Y;
+
+    if (this.tiltEffect === "reverse") {
+      X = ((offsetX - (this.w/2)) / 3) / 3;
+      Y = (-(offsetY - (this.h/2)) / 3) / 3;
+    }
+
+    else if (this.tiltEffect === "normal") {
+      X = (-(offsetX - (this.w/2)) / 3) / 3;
+      Y = ((offsetY - (this.h/2)) / 3) / 3;
+    }
+
+    this.setProperty('--rY', X.toFixed(2));
+    this.setProperty('--rX', Y.toFixed(2));
+
+    this.setProperty('--bY', (10 - (X/4).toFixed(2)) + '%');
+    this.setProperty('--bX', (50 - (Y/4).toFixed(2)) + '%');
+  }
+
+  handleMouseEnter() {
+    this.mouseOnComponent = true;
+    this.container.classList.add("container--active");
+  }
+
+  handleMouseLeave() {
+    this.mouseOnComponent = false;
+    this.defaultStates();
+  }
+
+  defaultStates() {
+    this.container.classList.remove("container--active");
+    this.setProperty('--rY', 0);
+    this.setProperty('--rX', 0);
+    this.setProperty('--bY', '10%');
+    this.setProperty('--bX', '50%');
+  }
+
+  setProperty(p, v) {
+    return this.container.style.setProperty(p, v);
+  }
+
+  init() {
+    this.element.addEventListener('mousemove', this.handleMouseMove);
+    this.element.addEventListener('mouseenter', this.handleMouseEnter);
+    this.element.addEventListener('mouseleave', this.handleMouseLeave);
+  }
+
+}
+
+const $ = e => document.querySelector(e);
+
+const wrap2 = new parallaxTiltEffect({
+  element: $('.wrap--2'),
+  tiltEffect: 'normal'
+});
